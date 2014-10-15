dfd90cc1156154ebe5912048595493cfd86db8b1
bonzai133 <bonzai133@25a5e7e9-39bf-4c39-8c37-d9a7e39ed5ba>
2014-10-15 14:17:09 +0000

    Correction for wrong end date

--- teleinfo/teleinfo_aggr.py
+++ teleinfo/teleinfo_aggr.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 '''
 Created on 12 janv. 2014
 
@@ -13,7 +15,7 @@ import datetime
 
 #Logger
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-LOGCONF_PATH = os.path.join(ROOT_PATH, 'logging.conf')
+LOGCONF_PATH = os.path.join(ROOT_PATH, 'logging_teleinfo.conf')
 
 logging.config.fileConfig(LOGCONF_PATH)
 logger = logging.getLogger(__name__)
@@ -141,11 +143,11 @@ def doAggregation(db, dateStart, dateEnd):
 #FROM TeleinfoDaily as T1, (SELECT T2.counterId as cid2, date(T2.date) as d2, max(T2.indexBase) as m2 FROM TeleinfoDaily as T2 group by date(T2.date), T2.counterId)
 #where T1.counterId=cid2 and d1 = date(d2, '-1 day') group by date(T1.date), T1.counterId;
     query_insert = "INSERT OR REPLACE INTO TeleinfoByDay (dateDay, counterId, indexBase, value) "
-    query_select1 = "SELECT date(T1.date) as d1, T1.counterId, m2, m2 - max(T1.indexBase) as diff "
+    query_select1 = "SELECT date(T1.date, '+1 day') as d1, T1.counterId, m2, m2 - max(T1.indexBase) as diff "
     query_from1 = "FROM TeleinfoDaily as T1, "
     query_select2 = "SELECT T2.counterId as cid2, date(T2.date) as d2, max(T2.indexBase) as m2 "
     query_from2 = "FROM TeleinfoDaily as T2 where d2 between '%s' and '%s' group by date(T2.date), T2.counterId "
-    query_where = "WHERE T1.counterId=cid2 and d1 between '%s' and '%s' and d1=date(d2, '-1 day') "
+    query_where = "WHERE T1.counterId=cid2 and d1 between '%s' and '%s' and d1=date(d2) "
     query_groupby = "GROUP BY date(T1.date), T1.counterId"
     
     query = query_insert + query_select1 + query_from1 + \
@@ -179,13 +181,13 @@ def main():
         if not re.match("\d{4}-\d{2}-\d{2}", args.dateStart):
             parser.error("Start date format must be yyyy-MM-dd")
     else:
-        args.dateStart = (datetime.date.today() - datetime.timedelta(1)).strftime("%Y-%m-%d")
+        args.dateStart = (datetime.date.today() - datetime.timedelta(2)).strftime("%Y-%m-%d")
         
     if args.dateEnd is not None:
         if not re.match("\d{4}-\d{2}-\d{2}", args.dateEnd):
             parser.error("End date format must be yyyy-MM-dd")
     else:
-        args.dateEnd = datetime.date.today().strftime("%Y-%m-%d")
+        args.dateEnd = (datetime.date.today() - datetime.timedelta(1)).strftime("%Y-%m-%d")
 
     logger.debug("Computed Args: %s" % repr(args))
     process(args.dbFileName, args.dateStart, args.dateEnd, args.createTables)
