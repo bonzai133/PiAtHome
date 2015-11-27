0d74fc0e0e70723c4f5f315c49345f368fa97abf
bonzai133 <bonzai133@25a5e7e9-39bf-4c39-8c37-d9a7e39ed5ba>
2015-11-27 13:32:27 +0000

    Unit tests for Inverter connection

--- tests/tcp_server.py
+++ tests/tcp_server.py
@@ -6,30 +6,45 @@ Created on Wed Nov 25 21:24:29 2015
 """
 
 import socket
+from multiprocessing import Process
 
-TCP_PORT = 12345
-BUFFER_SIZE = 255
-
-def server():
-    print "Start"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("127.0.0.1", TCP_PORT))
-    s.listen(1)
+class TcpServer():
+    def __init__(self, tcp_port=12345, buffer_size=255):
+        self.tcp_port = tcp_port
+        self.buffer_size = buffer_size
+        self.stop=False
     
-    conn, addr = s.accept()
-    print 'Connection address:', addr
-    while 1:
-        data = conn.recv(BUFFER_SIZE)
-        if not data:
-            break
-        print "received data:", data
-        conn.send("world")  # echo
-    conn.close()
-    print "End"
-
+    def serveOnceThread(self):
+        server = Process(target=self.serveOnce())
+        server.start()
+        
+    def serveOnce(self):
+        print "Start"
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("127.0.0.1", self.tcp_port))
+        s.listen(1)
+        
+        conn, addr = s.accept()
+        print 'Connection address:', addr
+        while 1:
+            data = conn.recv(self.buffer_size)
+            if not data:
+                break
+            print "received data:", data
+            conn.send("world")  # echo
+        conn.close()
+        print "End"
+    
+    def serveForEver(self):
+        while self.stop != True:
+            self.serveOnce()
+        
+    def stopServer(self):
+        self.stop = True
+    
 def main():
-    while 1:
-        server()
+    server = TcpServer()
+    server.serveForEver()
 
 if __name__ == '__main__':
     main()
