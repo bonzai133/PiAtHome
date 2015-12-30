98e2211031cec55f6a6c72e78b6996e55a850437
bonzai133 <bonzai133@25a5e7e9-39bf-4c39-8c37-d9a7e39ed5ba>
2015-12-30 15:47:12 +0000

    Update wsgi and start_server script

---
+++ PiAtHome/bin/Start_server.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import argparse
+
+from bottle import default_app, ServerAdapter, run, install, TEMPLATE_PATH
+from bottle_sqlite import SQLitePlugin
+from beaker.middleware import SessionMiddleware
+
+from requestlogger import WSGILogger, ApacheFormatter
+from logging.handlers import TimedRotatingFileHandler
+
+#Set file path
+ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../piathome')
+sys.path.append(ROOT_PATH)
+
+#Import main app
+import Charts
+
+#Define absolute path of templates for bottle
+TEMPLATE_PATH.insert(0, os.path.join(ROOT_PATH, "templates"))
+
+DB_FILE_SOLAR = os.path.join("/opt/pysolarmax/data", "Solarmax_data2.s3db")
+DB_FILE_TELEINFO = os.path.join("/opt/pysolarmax/data", "Teleinfo_data.s3db")
+VAR_LOG_ACCESS = os.path.join("/var/log", "access.log")
+VAR_LOG_ACCESS_SSL = os.path.join("/var/log", "access_ssl.log")
+
+#SSL Certificate
+SSL_CERTIFICATE = os.path.join(ROOT_PATH, "ssl/cacert.pem")
+SSL_PRIVATE_KEY = os.path.join(ROOT_PATH, "ssl/privkey.pem")
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+#Create sub class to handle SSL in cherrypy : require python-cherrypy3
+class SSLCherryPyServer(ServerAdapter):
+    def run(self, handler):
+        from cherrypy import wsgiserver
+        server = wsgiserver.CherryPyWSGIServer((self.host, self.port), handler)
+        server.ssl_certificate = SSL_CERTIFICATE
+        server.ssl_private_key = SSL_PRIVATE_KEY
+        try:
+            server.start()
+        finally:
+            server.stop()
+
+
+#------------------------------------------------------------------------------
+# main : start the right server
+#------------------------------------------------------------------------------
+def main(port):
+    #Beaker options
+    session_opts = {
+        'session.type': 'file',
+        'session.cookie_expires': True,
+        'session.data_dir': './.cache',
+        'session.auto': True
+    }
+    
+    #Debug mode ?
+    if port != 80 and port != 443:
+        #Sqlite db file
+        mydbfile_solarmax = os.path.join(ROOT_PATH, "../data/Solarmax_data2.s3db")
+        mydbfile_teleinfo = os.path.join(ROOT_PATH, "../data/Teleinfo_data.s3db")
+        access_log_file = 'access.log'
+
+        #Run http test server on given port
+        myserver = 'wsgiref'
+        
+    else:
+        #Sqlite db file
+        mydbfile_solarmax = DB_FILE_SOLAR
+        mydbfile_teleinfo = DB_FILE_TELEINFO
+        
+        #Run CherryPy http or https server
+        if port == 80:
+            myserver = 'cherrypy'
+            access_log_file = VAR_LOG_ACCESS
+        elif port == 443:
+            myserver = SSLCherryPyServer
+            access_log_file = VAR_LOG_ACCESS_SSL
+
+    #Create default bottle application
+    app = default_app()
+    myapp = SessionMiddleware(app, session_opts)
+
+    handlers = [TimedRotatingFileHandler(access_log_file, 'd', 7, 90), ]
+    loggingapp = WSGILogger(myapp, handlers, ApacheFormatter())
+    
+    #Plugins : SQLitePlugin give a connection in each functions with a db parameter
+    install(SQLitePlugin(dbfile=mydbfile_solarmax))
+    
+    plugin2 = SQLitePlugin(dbfile=mydbfile_teleinfo, keyword='db2')
+    plugin2.name = "sqlite2"
+    install(plugin2)
+    
+    #Run server
+    run(app=loggingapp, host='0.0.0.0', port=port, server=myserver, reload=True)
+
+
+if __name__ == "__main__":
+    #Get parameters
+    parser = argparse.ArgumentParser(description='Web server displaying charts for Solarmax and Teleinfo')
+
+    parser.add_argument('-p', '--port', dest='port', type=int, action='store',
+                        help='Listening port for the web server', default=8080)
+
+    args = parser.parse_args()
+
+    main(args.port)
--- PiAtHome/piathome/Charts.py
+++ PiAtHome/piathome/Charts.py
@@ -6,48 +6,14 @@ Created on 18 mars 2013
 @author: l.petit
 '''
 import os
-import sys
-import argparse
 
-from bottle import default_app, ServerAdapter, run, install, TEMPLATE_PATH
-from bottle import route, template, static_file, request, response, redirect, abort
-from bottle_sqlite import SQLitePlugin
-from beaker.middleware import SessionMiddleware
+from bottle import route, template, static_file, request, response, redirect
 
 from ApplicationUsers import ApplicationUsers
 
 from Charts_Authentication import *
 from Charts_Solar import *
 from Charts_Teleinfo import *
-
-from requestlogger import WSGILogger, ApacheFormatter
-from logging.handlers import TimedRotatingFileHandler
-
-
-#Set file path
-#ROOT_PATH = os.path.dirname(__file__)
-ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-#Define absolute path of templates for bottle
-TEMPLATE_PATH.insert(0, os.path.join(ROOT_PATH, "templates"))
-
-#DB_FILE = os.path.join(ROOT_PATH, "Solarmax_data2.s3db")
-DB_FILE_SOLAR = os.path.join("/opt/pysolarmax/data", "Solarmax_data2.s3db")
-DB_FILE_TELEINFO = os.path.join("/opt/pysolarmax/data", "Teleinfo_data.s3db")
-VAR_LOG_ACCESS = os.path.join("/var/log", "access.log")
-VAR_LOG_ACCESS_SSL = os.path.join("/var/log", "access_ssl.log")
-
-#SSL Certificate
-SSL_CERTIFICATE = os.path.join(ROOT_PATH, "ssl/cacert.pem")
-SSL_PRIVATE_KEY = os.path.join(ROOT_PATH, "ssl/privkey.pem")
-
-#Folders
-SCRIPTS_ROOT = os.path.join(ROOT_PATH, "scripts")
-SCRIPTS_CSS = os.path.join(ROOT_PATH, "css")
-SSL_FILES = os.path.join(ROOT_PATH, "ssl")
-IMAGES_FILES = os.path.join(ROOT_PATH, "images")
-
-USERSFILE = os.path.join(ROOT_PATH, "users.txt")
 
 
 #===============================================================================
@@ -57,6 +23,19 @@ USERSFILE = os.path.join(ROOT_PATH, "users.txt")
 # - Ajouter la valeur finale pour le cumul sur le graph
 # - Tableau avec ces infos en dessous du graph
 #
+
+#===============================================================================
+# Config
+#===============================================================================
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+USERSFILE = os.path.join(ROOT_PATH, "users.txt")
+                         
+#Folders
+SCRIPTS_ROOT = os.path.join(ROOT_PATH, "scripts")
+SCRIPTS_CSS = os.path.join(ROOT_PATH, "css")
+SSL_FILES = os.path.join(ROOT_PATH, "ssl")
+IMAGES_FILES = os.path.join(ROOT_PATH, "images")
+
 
 #===============================================================================
 # Routes definitions
@@ -128,12 +107,6 @@ def prod_realtime():
     return template("realtime", title="Pi@Home", login=getLogin())
 
 
-#@route("/prod_historic")
-#@authenticated
-#def prod_historic():
-#    return template("historic", title="Pi@Home", login=getLogin())
-
-
 @route("/prod_statistics")
 @authenticated
 def prod_statistics():
@@ -168,86 +141,3 @@ def send_css(filename):
 @route('/favicon.png')
 def send_favicon():
     return static_file('favicon.png', root=IMAGES_FILES)
-
-
-#===============================================================================
-# MAIN
-#===============================================================================
-#Create sub class to handle SSL in cherrypy : require python-cherrypy3
-class SSLCherryPyServer(ServerAdapter):
-    def run(self, handler):
-        from cherrypy import wsgiserver
-        server = wsgiserver.CherryPyWSGIServer((self.host, self.port), handler)
-        server.ssl_certificate = SSL_CERTIFICATE
-        server.ssl_private_key = SSL_PRIVATE_KEY
-        try:
-            server.start()
-        finally:
-            server.stop()
-
-
-#------------------------------------------------------------------------------
-# main : start the right server
-#------------------------------------------------------------------------------
-def main(port):
-    #Beaker options
-    session_opts = {
-      'session.type': 'file',
-      'session.cookie_expires': True,
-      'session.data_dir': './.cache',
-      'session.auto': True
-    }
-    
-    #Debug mode ?
-    if port != 80 and port != 443:
-        #Sqlite db file
-        #mydbfile_solarmax = os.path.join(ROOT_PATH, "Solarmax_data2.s3db")
-        #mydbfile_teleinfo = os.path.join(ROOT_PATH, "../teleinfo/Teleinfo_data.s3db")
-        mydbfile_solarmax = os.path.join(ROOT_PATH, "../data/Solarmax_data2.s3db")
-        mydbfile_teleinfo = os.path.join(ROOT_PATH, "../data/Teleinfo_data.s3db")
-        access_log_file = 'access.log'
-
-        #Run http test server on given port
-        myserver = 'wsgiref'
-        
-    else:
-        #Sqlite db file
-        mydbfile_solarmax = DB_FILE_SOLAR
-        mydbfile_teleinfo = DB_FILE_TELEINFO
-        
-        #Run CherryPy http or https server
-        if port == 80:
-            myserver = 'cherrypy'
-            access_log_file = VAR_LOG_ACCESS
-        elif port == 443:
-            myserver = SSLCherryPyServer
-            access_log_file = VAR_LOG_ACCESS_SSL
-
-    #Create default bottle application
-    app = default_app()
-    myapp = SessionMiddleware(app, session_opts)
-
-    handlers = [TimedRotatingFileHandler(access_log_file, 'd', 7, 90), ]
-    loggingapp = WSGILogger(myapp, handlers, ApacheFormatter())
-    
-    #Plugins : SQLitePlugin give a connection in each functions with a db parameter
-    install(SQLitePlugin(dbfile=mydbfile_solarmax))
-    
-    plugin2 = SQLitePlugin(dbfile=mydbfile_teleinfo, keyword='db2')
-    plugin2.name = "sqlite2"
-    install(plugin2)
-    
-    #Run server
-    run(app=loggingapp, host='0.0.0.0', port=port, server=myserver, reload=True)
-
-
-if __name__ == "__main__":
-    #Get parameters
-    parser = argparse.ArgumentParser(description='Web server displaying charts for Solarmax and Teleinfo')
-
-    parser.add_argument('-p', '--port', dest='port', type=int, action='store',
-                        help='Listening port for the web server', default=8080)
-
-    args = parser.parse_args()
-
-    main(args.port)
